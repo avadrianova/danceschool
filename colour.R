673390fe74054a83e35949f98311432320d96a99
askharlina <kharlina.arina@mail.ru>
2018-03-02 18:40:57 +0300

    ипирп

---
+++ colour.R
@@ -0,0 +1,54 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+#лялялялялялляляля
+# Define UI for application that draws a histogram 
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Old Faithful Geyser Data"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1, # мин место
+                     max = 50, # макс место, где стоит бегунок
+                     value = 30), # по умолчанию 
+         selectInput("col", "Цвета",choices = c("зелёный" = "green", 
+                                                "красный" = "red",
+                                                "синий" = "blue"))
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = input$col, border = 'white')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
